c23b2ce437b0c68043bdfdb595295809f5792272
Matheus Rosa <77069657+mnsgrosa@users.noreply.github.com>
2025-12-06 00:12:08 -0300

    Delete src/model/explainability_notebook directory

--- src/model/explainability_notebook/shap_explainer.py
+++
@@ -1,9 +0,0 @@
-import shap
-
-
-class Explainer:
-    def __init__(self, model, tokenizer):
-        self.explainer = shap.Explainer(model, tokenizer)
-
-    def explain(self, text):
-        return self.explainer(text)
